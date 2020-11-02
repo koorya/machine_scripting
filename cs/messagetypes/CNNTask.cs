5e9319f5872ab095b1d8a44f0e0bf1389f6f778a
Kirill Ivanov <puaktopblm@yandex.ru>
2020-11-02 15:34:01 +0700

    исправлена ошибка Задание для нейросети имеет только интерфейс request

--- cs/messagetypes/CNNTask.cs
+++ cs/messagetypes/CNNTask.cs
@@ -2,7 +2,7 @@ using OpenCvSharp;
 
 namespace json_converter
 {
-	public class CNNTask : Request, Response
+	public class CNNTask : Request
 	{
 		public int a { get; set; }
 		public int b { get; set; }
