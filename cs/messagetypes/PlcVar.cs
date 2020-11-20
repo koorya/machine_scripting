9a4e86d1121f86c09a4890bcfdd4661ef99131b3
Kirill Ivanov <puaktopblm@yandex.ru>
2020-11-20 23:31:26 +0700

    Добавил поле для внутреннего использования в программе.

--- cs/messagetypes/PlcVar.cs
+++ cs/messagetypes/PlcVar.cs
@@ -1,4 +1,6 @@
 
+using Newtonsoft.Json;
+
 namespace json_converter
 {
 	public class PlcVar
@@ -7,5 +9,8 @@ namespace json_converter
 		public string type { get; set; }
 		public string name { get; set; }
 		public object value { get; set; }
+		[JsonIgnore]
+		public object valueref { get; set; }
+
 	}
 }
