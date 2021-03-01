698702a6a66ad7dd31c05435c4057459de14ace2
Kirill Ivanov <puaktopblm@yandex.ru>
2021-03-01 16:01:47 +0700

    все кон троллеры читаются из файла, их переменные попадают в общий список, если удается подключиться к контроллеру в течение заданного времени 2с

--- MainApp/PlcConnector/PlcConnector.cs
+++ MainApp/PlcConnector/PlcConnector.cs
@@ -9,12 +9,12 @@ namespace PlcConnector_module
 {
 	// Объект, который создает и хранит список переменных манипулятора
 	// по файлу описания. В настоящий момент реализовано использование 
-  // переменных только одного манипулятора. Исходя из этого переменные 
+	// переменных только одного манипулятора. Исходя из этого переменные 
 	// имеют в качестве характеристики только собственное название,
 	// идентификации по разным контроллерам нет.
 	public class PlcConnector
 	{
-		public static PlcVar[] plc_vars;
+		public static PlcVar[] plc_vars = new PlcVar[0];
 		static PlcConnector()
 		{
 
@@ -29,7 +29,7 @@ namespace PlcConnector_module
 		}
 
 		static List<ExtCompolet> plc_conn; // список объектов для связи с плк через CIP
-		static public void connect(System.ComponentModel.IContainer cont)
+		static public async void connect(System.ComponentModel.IContainer cont)
 		{
 			plc_conn = new List<ExtCompolet>();
 
@@ -44,40 +44,56 @@ namespace PlcConnector_module
 				// добавляем этот объект в список объектов для связи с плк
 				plc_conn.Add(plc);
 			}
-			// ATTENTION только один плк используется, других вариантов пока не предусмотрено
-			var plc_con1 = plc_conn[0];
 
-			plc_con1.Active = true;
-			while (!plc_con1.IsConnected) ;
+			foreach (var plc_con__ in plc_conn)
+			{
+				plc_con__.Active = true;
+				int wait_time = 0;
+				while (!plc_con__.IsConnected && wait_time < 30)
+				{ //три секунды ожидаем
+					wait_time++;
+					await Task.Delay(100);
+				}
 
-			plc_vars = new PlcVar[plc_con1.plc_var_list.Count];
+				if (plc_con__.IsConnected)
+					Array.Resize(ref plc_vars, plc_vars.Length + plc_con__.plc_var_list.Count);
+				else
+					plc_con__.Active = false;
+			}
 
 			readPlcVariablesFromPLC();
 
 		}
 		public static void readPlcVariablesFromPLC()
 		{
-			// ATTENTION только один плк используется, других вариантов пока не предусмотрено
+
 			int i = 0;
-			foreach (var variable in plc_conn[0].plc_var_list)
+			foreach (var plc in plc_conn)
 			{
-				Console.WriteLine("variable for read: {0}",variable.Key);
-				variable.Value.readFromPlc();
-				Console.WriteLine(variable.Value.Plc_value);
+				if (plc.IsConnected)
+				{
 
-				plc_vars[i] = new PlcVar();
-				plc_vars[i].id = i;
-				plc_vars[i].name = variable.Value.name;
-				plc_vars[i].value = variable.Value.Plc_value;
-				plc_vars[i].type = getMyType(variable.Value.Plc_value?.GetType());
-				plc_vars[i].valueref = variable.Value;
-				i++;
+					foreach (var variable in plc.plc_var_list)
+					{
+						Console.WriteLine("variable for read: {0}", variable.Key);
+						variable.Value.readFromPlc();
+						Console.WriteLine(variable.Value.Plc_value);
 
+						plc_vars[i] = new PlcVar();
+						plc_vars[i].id = i;
+						plc_vars[i].name = variable.Value.name;
+						plc_vars[i].value = variable.Value.Plc_value;
+						plc_vars[i].type = getMyType(variable.Value.Plc_value?.GetType());
+						plc_vars[i].valueref = variable.Value;
+						i++;
+
+					}
+				}
 			}
 		}
 		public static void updatePlcVariablesByArray(PlcVar[] var_arr)
 		{
-			foreach(var upd_var in var_arr)
+			foreach (var upd_var in var_arr)
 			{
 				plcvariable plc_var_ref = plc_vars[upd_var.id].valueref as plcvariable;
 				plc_var_ref.Plc_value = upd_var.value;
