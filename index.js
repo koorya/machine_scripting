6c9c6b711bbc10dd8ade2ce04813035b833d5f24
Kirill Ivanov <puaktopblm@yandex.ru>
2021-07-21 18:35:49 +0700

    complete adding states

--- index.js
+++ index.js
@@ -38,6 +38,99 @@ const ext_transitions = [
   },
   { name: "step", from: "holding_frame_cycle", to: "holding_frame" },
   {
+    name: "prepare_to_lifting_bottom_frame",
+    from: "holding_frame",
+    to: "prepareing_to_lifting_bottom_frame",
+  },
+  {
+    name: "prepare_to_top_frame_moveing",
+    from: "holding_frame",
+    to: "prepareing_to_top_frame_moveing",
+  },
+  {
+    name: "step",
+    from: "prepareing_to_top_frame_moveing",
+    to: "up_limit",
+  },
+  {
+    name: "step",
+    from: "prepareing_to_top_frame_moveing",
+    to: "on_pins",
+  },
+  {
+    name: "step",
+    from: "prepareing_to_top_frame_moveing",
+    to: "on_pins_only",
+  },
+  {
+    name: "step",
+    from: "prepareing_to_lifting_bottom_frame",
+    to: "on_external_support",
+    dot: { tailport: "s", headport: "n" },
+  },
+  {
+    name: "pushin_crab",
+    from: "on_external_support",
+    to: "pushing_in_crab_cycle",
+  },
+  {
+    name: "step",
+    from: "pushing_in_crab_cycle",
+    to: "ready_to_lifting_bottom_frame",
+  },
+  {
+    name: "pushout_crab",
+    from: "ready_to_lifting_bottom_frame",
+    to: "pushing_out_crab_cycle",
+  },
+  {
+    name: "step",
+    from: "pushing_out_crab_cycle",
+    to: "on_external_support",
+  },
+  {
+    name: "liftUpBottomFrame",
+    from: "ready_to_lifting_bottom_frame",
+    to: "lifting_up_bottom_frame_cycle",
+  },
+  {
+    name: "step",
+    from: "lifting_up_bottom_frame_cycle",
+    to: "ready_to_lifting_bottom_frame",
+  },
+  {
+    name: "step",
+    from: "lifting_down_bottom_frame_cycle",
+    to: "ready_to_lifting_bottom_frame",
+  },
+  {
+    name: "liftDownBottomFrame",
+    from: "ready_to_lifting_bottom_frame",
+    to: "lifting_down_bottom_frame_cycle",
+  },
+  {
+    name: "land_bottom_frame_to_pins",
+    from: "on_external_support",
+    to: "landing_bottom_frame_to_pins",
+  },
+  {
+    name: "step",
+    from: "landing_bottom_frame_to_pins",
+    to: "on_pins",
+  },
+
+  {
+    name: "horizontal_move_top_frame",
+    from: "holding_frame",
+    to: "horizontal_moveing_cycle",
+  },
+  {
+    name: "step",
+    from: "horizontal_moveing_cycle",
+    to: "holding_frame",
+  },
+
+  {
     name: "liftDownFrame",
     from: ["up_limit", "on_pins_only"],
     to: "liftingDownFrame",
@@ -46,6 +139,11 @@ const ext_transitions = [
     name: "step",
     from: "liftingDownFrame",
     to: "on_pins_only",
+  },
+  {
+    name: "step",
+    from: "liftingDownFrame",
+    to: "on_pins",
   },
   {
     name: "gotoNextState",
@@ -130,7 +228,14 @@ var fsm = new StateMachine({
 
 function updateImage() {
   let tran = [...ext_transitions];
+  tran.map((edge) => {
+    if (edge.name === "step") {
+      if (!edge["dot"]) edge["dot"] = { color: "blue" };
+      else edge.dot["color"] = "blue";
 
+      edge.name = " ";
+    }
+  });
   var fsm_image = new StateMachine({
     init: "on_pins",
     transitions: tran,
@@ -178,4 +283,5 @@ let cmd_exec = setInterval(() => {
   }
 }, 1000);
 
-setInterval(() => updateImage(), 300);
+updateImage();
+// setInterval(() => updateImage(), 300);
