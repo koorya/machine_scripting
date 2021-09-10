110d9bcea36f33b752833e954f7bf88b57449330
Kirill Ivanov <puaktopblm@yandex.ru>
2021-09-10 19:40:02 +0700

    Добавлено поле update в объект с массивом переменных. Таким образом можно как читать так и записывать переменный одинаковым по структуре запросом, а не только читать все переменные сразу, как ранее

--- cs/messagetypes/PlcVarsArray.cs
+++ cs/messagetypes/PlcVarsArray.cs
@@ -1,16 +1,17 @@
 namespace json_converter
 {
 
-	public class PlcVarsArray: iRequest, iResponse
+	public class PlcVarsArray : iRequest, iResponse
 	{
 		public PlcVar[] arr { get; set; }
+		public bool update { get; set; } = true;
 		public PlcVarsArray()
 		{
-			
+
 		}
 		public object getWrapper()
 		{
-			return new {PlcVarsArray = this};
+			return new { PlcVarsArray = this };
 		}
 	}
 }
