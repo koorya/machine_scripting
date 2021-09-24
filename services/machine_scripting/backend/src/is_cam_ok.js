05eb047f30a9d7a0f373e81bb4120f51ca04e3d6
Kirill Ivanov <puaktopblm@yandex.ru>
2021-09-25 01:14:58 +0700

    Добавил  скрипт фейкового подтверждения картинки

---
+++ services/machine_scripting/backend/src/is_cam_ok.js
@@ -0,0 +1,9 @@
+// var dialog = require('dialog');
+
+setTimeout(()=>console.log('Ok'), 3000);
+
+// // example, without setting title
+// dialog.warn('is cam ok',  function(exitCode){
+//     if (exitCode == 1) console.log('User closed window');
+//     if (exitCode == 0) console.log('Ok');
+// })
