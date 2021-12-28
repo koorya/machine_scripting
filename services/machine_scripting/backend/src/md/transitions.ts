a8f777a2c0fa46f6a55d7c02b28ab3dd57611779
Kirill Ivanov <puaktopblm@yandex.ru>
2021-12-28 17:50:05 +0700

    У состояний и команд есть человекочитаемы описания

--- services/machine_scripting/backend/src/md/transitions.ts
+++ services/machine_scripting/backend/src/md/transitions.ts
@@ -3,111 +3,129 @@ import { GraphOfStates, } from "../fsm_types"
 export const graph: GraphOfStates = {
   transitions: [
     {
-      "name": "liftUpFrame",
-      "from": "on_pins_support",
-      "to": "lifting_up_frame_cycle"
+      name: "liftUpFrame",
+      from: "on_pins_support",
+      to: "lifting_up_frame_cycle", comment: ""
     },
-    { "name": "step", "from": "lifting_up_frame_cycle", "to": "on_pins_support" },
+    { name: "step", from: "lifting_up_frame_cycle", to: "on_pins_support", comment: "" },
     {
-      "name": "holdFrame",
-      "from": "on_pins_support",
-      "to": "holding_frame_cycle"
+      name: "holdFrame",
+      from: "on_pins_support",
+      to: "holding_frame_cycle", comment: ""
     },
-    { "name": "step", "from": "holding_frame_cycle", "to": "holding_frame" },
+    { name: "step", from: "holding_frame_cycle", to: "holding_frame", comment: "" },
     {
-      "name": "prepareToLiftingBottomFrame",
-      "from": "holding_frame",
-      "to": "prepareing_to_lifting_bottom_frame_cycle"
-    },
-    {
-      "name": "prepareToTopFrameMoveing",
-      "from": "holding_frame",
-      "to": "prepareing_to_top_frame_moveing_vertical"
+      name: "prepareToLiftingBottomFrame",
+      from: "holding_frame",
+      to: "prepareing_to_lifting_bottom_frame_cycle", comment: ""
     },
     {
-      "name": "step",
-      "from": "prepareing_to_top_frame_moveing_vertical",
-      "to": "on_pins_support"
+      name: "prepareToTopFrameMoveing",
+      from: "holding_frame",
+      to: "prepareing_to_top_frame_moveing_vertical", comment: ""
     },
     {
-      "name": "step",
-      "from": "prepareing_to_lifting_bottom_frame_cycle",
-      "to": "on_external_support",
+      name: "step",
+      from: "prepareing_to_top_frame_moveing_vertical",
+      to: "on_pins_support", comment: ""
+    },
+    {
+      name: "step",
+      from: "prepareing_to_lifting_bottom_frame_cycle",
+      to: "on_external_support", comment: "",
       "dot": { "tailport": "s", "headport": "n" }
     },
     {
-      "name": "pushinCrab",
-      "from": "on_external_support",
-      "to": "pushing_in_crab_cycle"
+      name: "pushinCrab",
+      from: "on_external_support",
+      to: "pushing_in_crab_cycle", comment: ""
     },
     {
-      "name": "step",
-      "from": "pushing_in_crab_cycle",
-      "to": "ready_to_lifting_bottom_frame"
+      name: "step",
+      from: "pushing_in_crab_cycle",
+      to: "ready_to_lifting_bottom_frame", comment: ""
     },
     {
-      "name": "pushoutCrab",
-      "from": "ready_to_lifting_bottom_frame",
-      "to": "pushing_out_crab_cycle"
+      name: "pushoutCrab",
+      from: "ready_to_lifting_bottom_frame",
+      to: "pushing_out_crab_cycle", comment: ""
     },
     {
-      "name": "step",
-      "from": "pushing_out_crab_cycle",
-      "to": "on_external_support"
+      name: "step",
+      from: "pushing_out_crab_cycle",
+      to: "on_external_support", comment: ""
     },
     {
-      "name": "liftUpBottomFrame",
-      "from": "ready_to_lifting_bottom_frame",
-      "to": "lifting_up_bottom_frame_cycle"
+      name: "liftUpBottomFrame",
+      from: "ready_to_lifting_bottom_frame",
+      to: "lifting_up_bottom_frame_cycle", comment: ""
     },
     {
-      "name": "step",
-      "from": "lifting_up_bottom_frame_cycle",
-      "to": "ready_to_lifting_bottom_frame"
+      name: "step",
+      from: "lifting_up_bottom_frame_cycle",
+      to: "ready_to_lifting_bottom_frame", comment: ""
     },
     {
-      "name": "step",
-      "from": "lifting_down_bottom_frame_cycle",
-      "to": "ready_to_lifting_bottom_frame"
+      name: "step",
+      from: "lifting_down_bottom_frame_cycle",
+      to: "ready_to_lifting_bottom_frame", comment: ""
     },
     {
-      "name": "liftDownBottomFrame",
-      "from": "ready_to_lifting_bottom_frame",
-      "to": "lifting_down_bottom_frame_cycle"
+      name: "liftDownBottomFrame",
+      from: "ready_to_lifting_bottom_frame",
+      to: "lifting_down_bottom_frame_cycle", comment: ""
     },
     {
-      "name": "landBottomFrameToPins",
-      "from": "on_external_support",
-      "to": "landing_bottom_frame_to_pins"
+      name: "landBottomFrameToPins",
+      from: "on_external_support",
+      to: "landing_bottom_frame_to_pins", comment: ""
     },
     {
-      "name": "step",
-      "from": "landing_bottom_frame_to_pins",
-      "to": "on_pins_support"
-    },
-
-    {
-      "name": "horizontalMoveTopFrame",
-      "from": "holding_frame",
-      "to": "horizontal_moveing_cycle"
-    },
-    {
-      "name": "step",
-      "from": "horizontal_moveing_cycle",
-      "to": "holding_frame"
+      name: "step",
+      from: "landing_bottom_frame_to_pins",
+      to: "on_pins_support", comment: ""
     },
 
     {
-      "name": "liftDownFrame",
-      "from": "on_pins_support",
-      "to": "lifting_down_frame_cycle"
+      name: "horizontalMoveTopFrame",
+      from: "holding_frame",
+      to: "horizontal_moveing_cycle", comment: ""
     },
     {
-      "name": "step",
-      "from": "lifting_down_frame_cycle",
-      "to": "on_pins_support"
+      name: "step",
+      from: "horizontal_moveing_cycle",
+      to: "holding_frame", comment: ""
+    },
+
+    {
+      name: "liftDownFrame",
+      from: "on_pins_support",
+      to: "lifting_down_frame_cycle", comment: ""
+    },
+    {
+      name: "step",
+      from: "lifting_down_frame_cycle",
+      to: "on_pins_support", comment: ""
     }
   ],
-  states: []
+  states: [
+    { name: "on_pins_support", comment: "На цапфах нижнего этажа" },
+    { name: "lifting_up_frame_cycle", comment: "Подъем на одну ступень" },
+    { name: "holding_frame_cycle", comment: "Удержание рамы" },
+    { name: "prepareing_to_top_frame_moveing_vertical", comment: "" },
+    { name: "prepareing_to_lifting_bottom_frame_cycle", comment: "" },
+    { name: "pushing_in_crab_cycle", comment: "Задвигание лап краба" },
+    { name: "pushing_out_crab_cycle", comment: "Выдвижение лап краба" },
+    { name: "lifting_up_bottom_frame_cycle", comment: "Подъем краба на<br/> одну ступень" },
+    { name: "lifting_down_bottom_frame_cycle", comment: "Спуск краба на одну ступень" },
+    { name: "ready_to_lifting_bottom_frame", comment: "Готов к подъему краба" },
+    { name: "on_external_support", comment: "Упирается на колонны рамой" },
+    { name: "landing_bottom_frame_to_pins", comment: "Посадка краба а цапфы" },
+    { name: "horizontal_moveing_cycle", comment: "Смещение по горизонтали" },
+    { name: "lifting_down_frame_cycle", comment: "Спуск на одну ступень" },
+
+
+
+  ]
 }
 
--- services/machine_scripting/backend/src/mp/transitions.ts
+++ services/machine_scripting/backend/src/mp/transitions.ts
@@ -2,18 +2,18 @@ import { GraphOfStates, } from "../fsm_types"
 
 export const graph: GraphOfStates = {
   transitions: [
-    { name: "moveUp", from: "bottom", to: "lifting_up", comment: "привет" },
-    { name: "moveDown", from: "top", to: "lifting_down", comment: "привет" },
-    { name: "step", from: "lifting_up", to: "top", comment: "привет" },
-    { name: "step", from: "lifting_down", to: "bottom", comment: "привет" },
-    { name: "moveDownEXTRA", from: "lifting_up", to: "lifting_down", comment: "привет" },
-    { name: "moveUpEXTRA", from: "lifting_down", to: "lifting_up", comment: "привет" }
+    { name: "moveUp", from: "bottom", to: "lifting_up", comment: "Вверх" },
+    { name: "moveDown", from: "top", to: "lifting_down", comment: "Вниз" },
+    { name: "step", from: "lifting_up", to: "top", comment: "" },
+    { name: "step", from: "lifting_down", to: "bottom", comment: "" },
+    { name: "moveDownEXTRA", from: "lifting_up", to: "lifting_down", comment: "Вниз(при ошибке)" },
+    { name: "moveUpEXTRA", from: "lifting_down", to: "lifting_up", comment: "Вверх(при ошибке)" }
   ],
   states: [
-    { name: "lifting_up", comment: "привет" },
-    { name: "lifting_down", comment: "привет" },
-    { name: "top", comment: "привет" },
-    { name: "bottom", comment: "привет" },
+    { name: "lifting_up", comment: "едет вверх" },
+    { name: "lifting_down", comment: "едет вниз" },
+    { name: "top", comment: "полностю поднят" },
+    { name: "bottom", comment: "полностью спущен" },
   ]
 };
 
