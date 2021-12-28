ae1e8e384144812063106f68c1362e1a05184dca
Kirill Ivanov <puaktopblm@yandex.ru>
2021-12-28 17:50:05 +0700

    У состояний и команд есть человекочитаемы описания

--- services/machine_scripting/backend/src/image_render.ts
+++ services/machine_scripting/backend/src/image_render.ts
@@ -1,4 +1,6 @@
 import * as graphviz from "graphviz";
+import { Edge, Graph } from "graphviz";
+
 import * as visualize from "javascript-state-machine/lib/visualize.js";
 import * as StateMachine from "javascript-state-machine";
 import { GraphOfStates } from "./fsm_types";
@@ -6,14 +8,17 @@ import { GraphOfStates } from "./fsm_types";
 export class ImageRender {
   rendered_image: string = null;
   dot_script: string;
+  graph: Graph;
   start_time: Date;
   constructor(graph: GraphOfStates) {
     const transitions = [...graph.transitions];
-    transitions.map((edge) => {
-      if (edge.name === "step") {
-        if (!edge["dot"]) edge["dot"] = { color: "blue" };
-        else edge.dot["color"] = "blue";
-        edge.name = " ";
+    transitions.map((transition) => {
+      if (!transition["dot"]) transition["dot"] = { color: "black" };
+      if (transition.comment && transition.comment != "") transition.dot = { ...transition.dot, label: `${transition.comment}` };
+      if (transition.name === "step") {
+        if (!transition["dot"]) transition["dot"] = { color: "blue" };
+        else transition.dot["color"] = "blue";
+        transition.name = " ";
       }
     });
     const init_state = transitions[0].from;
@@ -23,21 +28,40 @@ export class ImageRender {
     });
     this.dot_script = visualize(fsm_image, { orientation: "vertical" });
 
+    graphviz.parse(
+      this.dot_script, (gg) => {
+        gg.setNodeAttribut("shape", "rect");
+        gg.setNodeAttribut("style", "rounded");
+        graph.states.map(state => gg.getNode(state.name).set("label", `! <B> ${state.name} </B> <BR/> ${state.comment} `));
+        this.graph = gg;
+      });
+
     this.updateImage(init_state);
   }
+
   updateImage(active_node_name: string) {
     // нужно отменить предыдущее рисование, если вызвано снова
     const local_time = new Date();
     this.start_time = local_time;
-    graphviz.parse(this.dot_script, (gg) => {
-      if (this.start_time.getTime() != local_time.getTime()) return;
-      gg.getNode(active_node_name).set("color", "red");
-      gg.output("svg", (buff) => {
-        const local_image = buff.toString("base64");
-        if (this.start_time.getTime() == local_time.getTime())
-          this.rendered_image = local_image;
-      });
-      console.log("rendered");
+
+    const gg = this.graph;
+    if (!gg) {
+      setTimeout(() => this.updateImage(active_node_name), 100);
+      return;
+    }
+
+
+    if (this.start_time.getTime() != local_time.getTime()) return;
+    gg.getNode(active_node_name).set("color", "red");
+    gg.output("svg", (buff) => {
+      gg.getNode(active_node_name).set("color", "black");
+
+      const local_image = buff.toString("base64");
+      if (this.start_time.getTime() == local_time.getTime())
+        this.rendered_image = local_image;
     });
+    console.log("rendered");
+
+
   }
 }
