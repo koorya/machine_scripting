0e5e21be2fd404651d4b1f6d22fee8620112e6cf
Kirill Ivanov <puaktopblm@yandex.ru>
2022-02-04 19:31:42 +0700

    Удалил ненужный копипаст

--- services/machine_scripting/shared/commands/ext_commands.ts
+++ services/machine_scripting/shared/commands/ext_commands.ts
@@ -1,24 +1,8 @@
 import { MM_address } from "../types/types"
 
-
-enum MD_LIFTMECH_CMDS {
-	stop = 0,
-	expand = 1,
-	reduce = 2,
-}
-enum MD_LIFTMECH_STATES {
-	holding = 0,
-	expanding = 200,
-	reducing = 300,
-	expand_end = 400,
-	reduce_end = 500,
-	error = 98,
-
-};
-
 const init_commands = {
 	setAddres: { cassete: 0, pos: 0 } as MM_address,
 
 };
 
-export { init_commands, MD_LIFTMECH_CMDS, MD_LIFTMECH_STATES }
+export { init_commands }
