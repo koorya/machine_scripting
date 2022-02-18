6d69a40a8133d235d98f0746ec580c0fafecaa49
Kirill Ivanov <puaktopblm@yandex.ru>
2022-02-18 09:34:59 +0700

    Установить домкрат реальной машиной

--- services/machine_scripting/shared/config/machines_config.ts
+++ services/machine_scripting/shared/config/machines_config.ts
@@ -10,40 +10,40 @@ export const address_list: AddressListType[] = [
 	//   type: "MM",
 	//   photo: "photo address"
 	// },
-	{
-		name: "fake_mm",
-		zmq_port: 5554,
-		ui_port: 5003,
-		is_fake: true,
-		specific_params: {
-			type: "MM",
-			neuro: {
+	// {
+	// 	name: "fake_mm",
+	// 	zmq_port: 5554,
+	// 	ui_port: 5003,
+	// 	is_fake: true,
+	// 	specific_params: {
+	// 		type: "MM",
+	// 		neuro: {
 
-				ipcl: "172.16.201.137",
-				ipcr: "172.16.201.142",
-				port: 8090
-			}
-		},
-	},
-	{
-		name: "fake Подъемник",
-		zmq_port: 5556,
-		ui_port: 5006,
-		is_fake: true,
-		specific_params: {
-			type: "MP",
-			length: 11,
-			reading_port: { zmq: 5800, ui: 5810 },
-			seting_port: { zmq: 5801, ui: 5811 },
-		},
-	},
+	// 			ipcl: "172.16.201.137",
+	// 			ipcr: "172.16.201.142",
+	// 			port: 8090
+	// 		}
+	// 	},
+	// },
+	// {
+	// 	name: "fake Подъемник",
+	// 	zmq_port: 5556,
+	// 	ui_port: 5006,
+	// 	is_fake: true,
+	// 	specific_params: {
+	// 		type: "MP",
+	// 		length: 11,
+	// 		reading_port: { zmq: 5800, ui: 5810 },
+	// 		seting_port: { zmq: 5801, ui: 5811 },
+	// 	},
+	// },
 	{
 		name: "fake Домкрат",
 		zmq_port: 5555,
 		ui_port: 5004,
-		is_fake: true,
-		// is_fake: false,
-		// ip: "192.168.250.70",
+		// is_fake: true,
+		is_fake: false,
+		ip: "192.168.250.70",
 		specific_params: {
 			type: "MD",
 			hydro: 10,
@@ -51,17 +51,17 @@ export const address_list: AddressListType[] = [
 			seting_port: { zmq: 5701, ui: 5711 },
 		},
 	},
-	{
-		name: "мастер",
-		specific_params: {
-			type: "MASTER",
-			md_port: 5004,
-			mm_port: 5003,
-			mp_port: 5006
-		},
-		is_fake: false,
-		ip: "not used",
-		zmq_port: 5557,
-		ui_port: 5007,
-	}
+	// {
+	// 	name: "мастер",
+	// 	specific_params: {
+	// 		type: "MASTER",
+	// 		md_port: 5004,
+	// 		mm_port: 5003,
+	// 		mp_port: 5006
+	// 	},
+	// 	is_fake: false,
+	// 	ip: "not used",
+	// 	zmq_port: 5557,
+	// 	ui_port: 5007,
+	// }
 ];
