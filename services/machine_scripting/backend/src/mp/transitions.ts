995674d7013997b2ff8a0d1711275677d798b669
Kirill Ivanov <puaktopblm@yandex.ru>
2022-02-22 18:54:15 +0700

    Добавить состояние tension_control на граф МП

--- services/machine_scripting/backend/src/mp/transitions.ts
+++ services/machine_scripting/backend/src/mp/transitions.ts
@@ -3,6 +3,9 @@ import { GraphOfStates, } from "../fsm_types"
 export const root_graph = {
   transitions: [
     { name: "moveUp", from: "bottom", to: "lifting_up", comment: "Вверх" },
+    { name: "tensionEnable", from: "bottom", to: "tension_control" },
+    { name: "tensionDisable", from: "tension_control", to: "bottom" },
+
     { name: "moveDown", from: "top", to: "lifting_down", comment: "Вниз" },
     { name: "step", from: "lifting_up", to: "top", comment: "" },
     { name: "step", from: "lifting_down", to: "bottom", comment: "" },
@@ -14,6 +17,7 @@ export const root_graph = {
     { name: "lifting_down", comment: "едет вниз" },
     { name: "top", comment: "полностю поднят" },
     { name: "bottom", comment: "полностью спущен" },
+    { name: "tension_control", comment: "Слежение за натягом троса" }
   ],
   init: "bottom",
 } as const;
